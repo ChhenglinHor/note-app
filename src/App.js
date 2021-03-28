6f14393e59b1b5a5e3ab959b06e2efae50b6b4de
chhenglin <hchhenglin@gmail.com>
2021-03-28 19:53:49 +0700

    add modal form for creating new note

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import "./App.css";
 import Description from "./component/Description";
 import TitleList from "./component/TitleList";
 import { createContext, useState } from "react";
+import { Button, Form, Input, Modal } from "antd";
 
 export const ListContext = createContext();
 
 function App() {
   const [idState, setIdState] = useState();
+  const [visible, setVisible] = useState(false);
+
+  const onCreate = (values) => {
+    console.log("Received values of form: ", values);
+    setVisible(false);
+  };
 
   const list = [
     { id: 0, value: "My Note1", des: "My note here is something about 1" },
@@ -14,23 +21,87 @@ function App() {
     { id: 2, value: "My Note3", des: "My note here is something about 3" },
   ];
 
-  console.log('idState', idState);
-  console.log('value', list[idState]);
+  console.log("idState", idState);
+  console.log("value", list[idState]);
+
+  const CollectionCreateForm = ({ visible, onCreate, onCancel }) => {
+    const [form] = Form.useForm();
+    return (
+      <Modal
+        visible={visible}
+        title="Create New Note"
+        okText="Create"
+        cancelText="Cancel"
+        onCancel={onCancel}
+        onOk={() => {
+          form
+            .validateFields()
+            .then((values) => {
+              form.resetFields();
+              onCreate(values);
+            })
+            .catch((info) => {
+              console.log("Validate Failed:", info);
+            });
+        }}
+      >
+        <Form form={form} layout="vertical" name="form_in_modal">
+          <Form.Item
+            name="title"
+            label="Title"
+            rules={[
+              {
+                required: true,
+                message: "Please input the title of collection!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item name="description" label="Description">
+            <Input type="textarea" />
+          </Form.Item>
+        </Form>
+      </Modal>
+    );
+  };
 
   return (
     <>
-      <ListContext.Provider value={list} >
+      <ListContext.Provider value={list}>
         <div className="App">
           <h1>My Note</h1>
         </div>
         <div className="flex-container">
           <div className="flex-child">
-            <TitleList className="titleListContainer" handleClick={(id) => setIdState(id)} />
+            <TitleList
+              className="titleListContainer"
+              handleClick={(id) => setIdState(id)}
+            />
+            <Button
+              style={{ marginTop: "10px" }}
+              onClick={() => {
+                setVisible(true);
+              }}
+            >
+              Add New Note
+            </Button>
           </div>
           <div className="flex-child">
-            <Description className="descriptionContainer" title={list[idState]?.value} description={list[idState]?.des} />
+            <Description
+              className="descriptionContainer"
+              title={list[idState]?.value}
+              description={list[idState]?.des}
+            />
           </div>
         </div>
+        <CollectionCreateForm
+          visible={visible}
+          onCreate={onCreate}
+          onCancel={() => {
+            setVisible(false);
+          }}
+        />
       </ListContext.Provider>
     </>
   );
